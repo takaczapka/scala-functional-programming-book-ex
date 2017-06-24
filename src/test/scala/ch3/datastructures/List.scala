f3d6fe12b390560e8ea16373c1dce64b3fa1e39f
Maciej Falski <takaczapka@gmail.com>
2017-06-24 17:28:45 +0100

    more List related functions (folds, map, flatMap, etc.)

--- src/test/scala/ch3/datastructures/List.scala
+++ src/test/scala/ch3/datastructures/List.scala
@@ -4,6 +4,8 @@ import java.util.NoSuchElementException
 
 import org.scalatest.{FunSuite, Matchers}
 
+import scala.annotation.tailrec
+
 trait List[+A] {
   def tail: List[A]
 
@@ -20,9 +22,26 @@ trait List[+A] {
   def foldLeft[B](z: B)(f: (B, A) => B): B
 
   def length: Int
+
   def length2: Int
 
   def reverse: List[A]
+
+  def append[B >: A](l: List[B]): List[B]
+
+  def append2[B >: A](l: List[B]): List[B]
+
+  def map[B](f: A => B): List[B]
+
+  def filter(f: A => Boolean): List[A]
+
+  def filter2(f: A => Boolean): List[A]
+
+  def flatMap[B >: A](f: B => List[B]): List[B]
+
+  def zipWith[B >: A](l : List[B])(f: (A, B) => B): List[B]
+
+  def hasSubsequence[B >: A](l: List[B]): Boolean
 }
 
 case object Nil extends List[Nothing] {
@@ -41,9 +60,26 @@ case object Nil extends List[Nothing] {
   override def foldLeft[B](z: B)(f: (B, Nothing) => B): B = z
 
   override def length: Int = 0
+
   override def length2: Int = 0
 
   override def reverse: List[Nothing] = this
+
+  override def append[B](l: List[B]): List[B] = l
+
+  override def append2[B](l: List[B]): List[B] = l
+
+  override def map[B](f: (Nothing) => B): List[B] = Nil
+
+  override def filter(f: (Nothing) => Boolean): List[Nothing] = Nil
+
+  override def filter2(f: (Nothing) => Boolean): List[Nothing] = Nil
+
+  override def flatMap[B >: Nothing](f: (B) => List[B]): List[B] = Nil
+
+  override def zipWith[B >: Nothing](l: List[B])(f: (Nothing, B) => B): List[B] = Nil
+
+  override def hasSubsequence[B >: Nothing](l: List[B]): Boolean = if (l == Nil) true else false
 }
 
 case class Cons[+A](head: A, tail: List[A]) extends List[A] {
@@ -66,7 +102,36 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A] {
 
   override def length2: Int = foldLeft(0)((b, _) => b + 1)
 
-  override def reverse: List[A] = foldLeft(Nil:List[A])((b, a) => Cons(a, b))
+  override def reverse: List[A] = foldLeft(Nil: List[A])((b, a) => Cons(a, b))
+
+  override def append[B >: A](l: List[B]): List[B] = reverse.foldLeft(l) { case (acc, c) =>
+    Cons(c, acc)
+  }
+
+  override def append2[B >: A](l: List[B]): List[B] = foldRight(l) { case (c, acc) => Cons(c, acc) }
+
+  override def map[B](f: A => B): List[B] = Cons(f(head), tail.map(f))
+
+  override def filter(f: (A) => Boolean): List[A] = if (f(head)) Cons(head, tail.filter(f)) else tail.filter(f)
+
+  override def filter2(f: (A) => Boolean): List[A] = flatMap(a => if (f(a)) Cons(a, Nil) else Nil)
+
+  override def flatMap[B >: A](f: (B) => List[B]): List[B] = List.concat(map(f))
+
+  override def zipWith[B >: A](l: List[B])(f: (A, B) => B): List[B] = l match {
+    case Cons(_head, _t) => Cons(f(head, _head), tail.zipWith(_t)(f))
+    case Nil             => Nil
+  }
+
+  override def hasSubsequence[B >: A](l: List[B]): Boolean =
+  {
+    def startsWith(l1: List[B], l2: List[B]): Boolean = (l1, l2) match {
+      case (_, Nil) => true
+      case (Cons(head1, tail1), Cons(head2, tail2)) => if (head1 == head2) startsWith(tail1, tail2) else false
+    }
+
+    if (startsWith(this, l)) true else tail.hasSubsequence(l)
+  }
 }
 
 object List {
@@ -88,6 +153,31 @@ object List {
   def product3(ds: List[Double]): Double = ds.foldLeft(1.0)(_ * _)
 
   def sum2(ds: List[Double]): Double = ds.foldLeft(0.0)(_ + _)
+
+  def foldRight[A, B](ds: List[A], z: B)(f: (A, B) => B): B = ds match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  @tailrec
+  def foldLeft[A, B](ds: List[A], z: B)(f: (B, A) => B): B = ds match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
+
+  def foldRight2[A, B](ds: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(ds.reverse, z)((a, b) => f(b, a))
+
+  def foldLeft2[A, B](ds: List[A], z: B)(f: (B, A) => B): B =
+    foldRight(ds.reverse, z)((a, b) => f(b, a))
+
+  def :\[A, B](ds: List[A], z: B)(f: (A, B) => B): B = foldRight(ds, z)(f)
+
+  def /:[A, B](ds: List[A], z: B)(f: (B, A) => B): B = foldLeft(ds, z)(f)
+
+  def concat[A](lists: List[List[A]]): List[A] = foldLeft(lists, Nil: List[A]) { case (a, acc) =>
+    a.append(acc)
+  }
 }
 
 
@@ -185,5 +275,75 @@ class ListTest extends FunSuite with Matchers {
     Nil.reverse should be(Nil)
     List(1).reverse should be(List(1))
     List(1, 3, 5).reverse should be(List(5, 3, 1))
+  }
+
+  test("append") {
+    def append1[A](l1: List[A], l2: List[A]) = l1.append(l2)
+    def append2[A](l1: List[A], l2: List[A]) = l1.append2(l2)
+
+    def testAppend(append: (List[Int], List[Int]) => List[Int]) {
+      append(List(), List()) should be(List())
+      append(List(1), List()) should be(List(1))
+      append(List(), List(1)) should be(List(1))
+      append(List(1, 2), List(3, 4)) should be(List(1, 2, 3, 4))
+    }
+
+    testAppend(append1)
+    testAppend(append2)
+  }
+
+  test("concat") {
+    concat(List(List())) should be(List())
+    concat(List(List(1))) should be(List(1))
+    concat(List(List(1, 2))) should be(List(1, 2))
+    concat(List(List(1, 2), List(3))) should be(List(1, 2, 3))
+    concat(List(List(1, 2), List(3), List(4))) should be(List(1, 2, 3, 4))
+  }
+
+  test("map") {
+    List(1, 2, 3).map(_ + 1) should be(List(2, 3, 4))
+    List(1.0, 2.0, 3.0).map(_.toString) should be(List("1.0", "2.0", "3.0"))
+  }
+
+  test("filter") {
+    List(1, 2, 3).filter(_ > 2) should be(List(3))
+    List(1, 2, 3).filter(_ <= 2) should be(List(1, 2))
+  }
+
+  test("filter2") {
+    List(1, 2, 3).filter2(_ > 2) should be(List(3))
+    List(1, 2, 3).filter2(_ <= 2) should be(List(1, 2))
+  }
+
+  test("flatmap") {
+    List(1, 2, 3).flatMap(a => List(a, a)) should be(List(1, 1, 2, 2, 3, 3))
+  }
+
+  test("zipWith") {
+    List(1, 2, 3).zipWith(List())(_ + _) should be (List())
+    List(1, 2, 3).zipWith(List(1, 2))(_ + _) should be (List(2, 4))
+    List(1, 2, 3).zipWith(List(3, 4, 5))(_ + _) should be (List(4, 6, 8))
+    List(1).zipWith(List(1, 2))(_ + _) should be (List(2))
+    List[Int]().zipWith(List(1, 2))(_ + _) should be (List())
+  }
+
+  test("has subsequence") {
+    List().hasSubsequence(List()) should be(true)
+    List().hasSubsequence(List(1)) should be(false)
+    List(1).hasSubsequence(List(1)) should be(true)
+    List(2).hasSubsequence(List(1)) should be(false)
+
+    val l = List(1, 2, 3, 4)
+    l.hasSubsequence(List()) should be (true)
+    l.hasSubsequence(List(1)) should be (true)
+    l.hasSubsequence(List(2)) should be (true)
+    l.hasSubsequence(List(1, 2)) should be (true)
+    l.hasSubsequence(List(1, 2, 3)) should be (true)
+    l.hasSubsequence(List(1, 2, 4)) should be (false)
+    l.hasSubsequence(List(1, 2, 3, 4)) should be (true)
+    l.hasSubsequence(List(2, 3, 4)) should be (true)
+    l.hasSubsequence(List(3, 4)) should be (true)
+    l.hasSubsequence(List(4)) should be (true)
+    l.hasSubsequence(List(3)) should be (true)
   }
 }
