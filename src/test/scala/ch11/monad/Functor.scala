582bd06c3388562e7118c82f815275a84100315a
takaczapka <takaczapka@gmail.com>
2017-10-18 19:26:54 +0100

    ch11: Functor added

---
+++ src/test/scala/ch11/monad/Functor.scala
@@ -0,0 +1,21 @@
+package ch11.monad
+
+import ch4.errorhandling._
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+
+  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) =
+    (map(fab)(_._1), map(fab)(_._2))
+
+  def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+    case Left(fa) => map(fa)(Left(_))
+    case Right(fa) => map(fa)(Right(_))
+  }
+}
+
+object Functor {
+  val listFunction: Functor[List] = new Functor[List] {
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa map f
+  }
+}
